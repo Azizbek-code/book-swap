d9b31bcb31bf0de3a72a7f9e1a90b5faf97c5c87
elemahnovbahrombek <elemahnovbahrombek@gmail.com>
2026-01-06 22:17:05 +0500

    help

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
 import { CoreModule } from './core/core.module';
-import { PostsModule } from './modules/posts/posts.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { BooksModule } from './modules/books/books.module';
+import { PostsModule } from './modules/posts/posts.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [UsersModule, AuthModule, CoreModule, PostsModule, BooksModule],
+  
 })
 export class AppModule { }
--- src/modules/books/books.module.ts
+++ src/modules/books/books.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { checkUserDatas } from 'src/common/functions/chechUser';
 
 @Module({
   controllers: [BooksController],
-  providers: [BooksService],
+  providers: [BooksService,checkUserDatas],
 })
 export class BooksModule {}
